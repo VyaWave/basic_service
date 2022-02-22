63e0872e79ab3ad475a454ee187b79207d916580
weiya <wave.weiya@gmail.com>
2022-02-22 16:02:19 +0800

    feature: update logs

--- config/config.default.ts
+++ config/config.default.ts
@@ -13,6 +13,12 @@ export default (appInfo: EggAppInfo) => {
   // add your special config in here
   const bizConfig = {
     sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`,
+    logger: {
+      appLogName: `${appInfo.name}-web.log`,
+      coreLogName: 'egg-web.log',
+      agentLogName: 'egg-agent.log',
+      errorLogName: 'common-error.log',
+    },
   };
 
   // the return config will combines to EggAppConfig
